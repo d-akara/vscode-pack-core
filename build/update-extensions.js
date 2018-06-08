0d73dc892d37f4435a009361cae686f4dd541eb0
Dakara <chad.meadows@gmail.com>
2018-06-07 20:29:44 -0400

    removed unneeded filter

--- build/update-extensions.js
+++ build/update-extensions.js
@@ -5,10 +5,9 @@ const bundlePackage = require('../package.json');
 const EXTENSIONS_PATH = os.homedir() + '/.vscode/extensions'
 const OUT_FILE = '/vscode-extensions.txt'
 
-const packages = fs.find(EXTENSIONS_PATH, {matching: '!.*', directories: true, recursive: false})
-                    .filter(path => path.startsWith('.'))
-                    .map(packagePath => JSON.parse(fs.read(packagePath + '/package.json')))
-                    .sort((a,b) => a.displayName.localeCompare(b.displayName))
+const packages = fs.find(EXTENSIONS_PATH, {matching: '!.*', directories: true, recursive: false}) // All first level directories that don't start with '.'
+                    .map(packagePath => JSON.parse(fs.read(packagePath + '/package.json')))       // All package files as package JSON objects
+                    .sort((a,b) => a.displayName.localeCompare(b.displayName))                    // Sort all packages by name
 
 packages.forEach(package => {
     console.log(makeMarkDownLink(package))
